bcb16e0c0db7442d0421cb2b8830f303b0f855ff
Luis Lobo Borobia <luis.lobo@petrocloud.com>
2017-10-26 15:12:28 -0500

    Add check to see if values are set

--- code/shell/config_for_pico.sh
+++ code/shell/config_for_pico.sh
@@ -29,11 +29,29 @@ python setup.py install
 systemctl enable picofssd.service
 
 echo '--- adding line to config.txt'
-echo -e "\n\ndtoverlay=i2c-rtc,ds1307\n\n" >> /boot/config.txt
-echo -e "\n\nenable_uart=1\n\n" >> /boot/config.txt
+if [ "$(grep -c "^dtoverlay=i2c-rtc,ds1307" /boot/config.txt)" -eq 0 ]; then
+  echo -e "\ndtoverlay=i2c-rtc,ds1307\n" >> /boot/config.txt
+fi
+
+if [ "$(grep -c "^enable_uart=1" /boot/config.txt)" -eq 0 ]; then
+  echo -e "\nenable_uart=1\n" >> /boot/config.txt
+fi
+
+if [ "$(grep -c "^dtparam=i2c_arm=on" /boot/config.txt)" -eq 0 ]; then
+  echo -e "\ndtparam=i2c_arm=on\n" >> /boot/config.txt
+fi
+
+if [ "$(grep -c "^dtparam=i2c1=on" /boot/config.txt)" -eq 0 ]; then
+  echo -e "\ndtparam=i2c1=on\n" >> /boot/config.txt
+fi
 
 echo '--- adding lines to /etc/modules'
-echo -e "\n\ni2c-bcm2708\ni2c-dev\rtc-ds1307\n\n" >> /etc/modules
+if [ "$(grep -c "^i2c-bcm2708" /etc/modules)" -eq 0 ]; then
+  echo -e "\ni2c-bcm2708\n" >> /etc/modules
+fi
+if [ "$(grep -c "^rtc-ds1307" /etc/modules)" -eq 0 ]; then
+	echo -e "\nrtc-ds1307\n" >> /etc/modules
+fi
 
 echo '--- removing fake-hwclock'
 apt-get -y remove fake-hwclock && sudo update-rc.d -f fake-hwclock remove
