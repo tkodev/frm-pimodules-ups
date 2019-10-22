654189adcc73d385e442ec8e99987bebc93c1adb
Tony Ko <tony@htko.ca>
2019-10-22 16:31:56 -0400

    Do not use rpi-update due to pre-release status

--- shell_scripts/setup_dependencies.sh
+++ shell_scripts/setup_dependencies.sh
@@ -11,8 +11,8 @@
   sudo apt-get upgrade -y
   echo '--- dist-upgrade'
   sudo apt-get dist-upgrade -y
-  echo '--- rpi-update'
-  sudo rpi-update
+  # echo '--- rpi-update'
+  # sudo rpi-update
   echo '--- install some packages'
   sudo apt-get install -y git python-dev python-pip python-serial python-smbus python-jinja2 python-xmltodict python-psutil i2c-tools libi2c-dev
   echo '--- pip install rpi.gpio'
